5376a67be041be7d933c181db6d12dea27f5ee38
nicholastalty <nicholas.talty@bryanuniversity.edu>
2021-10-19 12:39:21 -0700

    Edited Week2 assignment

--- week2/assignments/calendar/calendar.js
+++ week2/assignments/calendar/calendar.js
@@ -6,42 +6,81 @@
 
 // modify this script to use the first day of the month the user selects in place of the const today 
 
-const today = new Date('5/1/2021')
-const month = today.getMonth()
-let days
-switch (month) {
-    case 1:
-        days = 28
-        break
-    case 3:
-    case 5:
-    case 8: 
-    case 10:
-        days = 30
-        break
-    default:
-        days = 31
-}
-    
-let x
-const weekday = today.getDay()
-for (x = 0; x < weekday; x++){
-    document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
-}
+const myMonths = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] 
 
-let dt = 0
-do {
-    dt++
-    document.getElementById('calendarDays').innerHTML += `<div class='calendarCell'>${dt}</div`
-} while ( dt < days)
+let monthOptions = myMonths.map((curMonth, curIndex) => 
+    `<option value=${curIndex+1}>${curMonth}</option>`
+);
 
-const monthName = today.toLocaleDateString('default', {month:'long'})
-const year = today.getFullYear()
-document.querySelector('.calendarTitle').innerText = `${monthName} ${year}`
+//console.log('monthOptions: ', monthOptions)
 
-const remainder = (7 - ((x + dt) % 7))
-let y = 0
-while ( y < remainder) {
-    document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
-    y++
+document.getElementById('calendarMonth').innerHTML = monthOptions
+
+document.getElementById('calendarPrint').addEventListener(
+    'click', function() {printCalendar()}
+)
+
+function printCalendar() {
+
+    document.getElementById('calendarDays').innerHTML = ''
+
+    //const today = new Date
+
+    //const month = today.getMonth()
+
+    const selMonth = document.getElementById('calendarMonth').value
+
+    const inputYear = document.getElementById('calendarYear').value
+
+    const calDate = `${selMonth}/1/${inputYear}`
+
+    const firstDate = new Date(calDate)
+
+    console.log('selected Month: ', selMonth)
+
+
+    let days
+    switch (parseInt(selMonth)) {
+        case 2:
+            days = 28
+            break
+        case 3:
+        case 5:
+        case 8: 
+        case 10:
+            days = 31
+            break
+        default:
+            days = 31
+    }
+
+    console.log('days: ', days)
+        
+    let x
+    //const weekday = today.getDay()
+    const weekday = firstDate.getDay()
+    for (x = 0; x < weekday; x++){
+        document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
+    }
+
+    let dt = 0
+    do {
+        dt++
+        document.getElementById('calendarDays').innerHTML += `<div class='calendarCell'>${dt}</div`
+    } while ( dt < days)
+
+    //const monthName = today.toLocaleDateString('default', {month:'long'})
+    //const year = today.getFullYear()
+
+    const monthName = firstDate.toLocaleDateString('default', {month:'long'})
+    const year = firstDate.getFullYear()
+
+    document.querySelector('.calendarTitle').innerText = `${monthName} ${year}`
+
+    const remainder = (7 - ((x + dt) % 7))
+    let y = 0
+    while ( y < remainder) {
+        document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
+        y++
+    }
 }
