365c1de53c58de858f11321bed81459aa7a857e3
nicholastalty <nicholas.talty@bryanuniversity.edu>
2021-11-02 19:52:13 -0700

    edited languages assignment

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -4,25 +4,26 @@ const lang = 'JavaScript'
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
 
+document.getElementById('submit').addEventListener('click', function(){
+    var inputsAll = document.querySelectorAll('input')
+    var ary = [...inputsAll]
+    checkLang(ary)
+})
 
-function chkLang(langs, ...values) {
+
+function checkLang(langs){
     let result = false
-    console.log(langs, values);
-    for (let i = 0; i < values.length; i++) {
-        console.log(values[i])
-         let item = values[i]
-        if (item == "JavaScript"){
+    for (let i = 0; i < langs.length; i++) {
+         let item = langs[i].value
+        if (item == lang){
             result = true;
         }
     };
 
-    // use an array method to check whether the user included 'JavaScript' in their
-    // list of languages
-
-    let obj = document.querySelector('#TestResult')
-    if (result) 
-        obj.innerText = `Congratulations!\nYou know ${lang}.`
-    else
-        obj.innerText = `Sorry,\nyou don't know ${lang}.`
+    let testRslt = document.querySelector('#TestResult')
+    if (result) {
+        testRslt.innerText = `Congratulations!\nYou know ${lang}.`
+    } else {
+        testRslt.innerText = `Sorry,\nyou don't know ${lang}.`
+    }
 }
-
