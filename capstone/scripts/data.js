31a94ad15389423e5e1661ac2839a60726bbbbdb
nicholastalty <nicholas.talty@bryanuniversity.edu>
2021-12-18 15:43:13 -0800

    added Warehouse assignment

--- capstone/scripts/data.js
+++ capstone/scripts/data.js
@@ -1,7 +1,9 @@
 // Call the "getSystems()" function in such a way that when the page loads, the "system" select element displays the three sytems 
 // whose parentID is zero.
 
-function getSystems() {
+getSystems(0, document.getElementById('system'))
+
+function getSystems(parentId, domElement) {
     // This function will accept two arguments (see week three): the parentID and the DOM element that will ultimately receive the data. 
     // Code this function as an asynchronous operation that will fetch data from the data.json file (see week three).
     // After receiving the JSON data, the asynchronous promise should use a higher order array method (see week two) to return only 
@@ -10,17 +12,43 @@ function getSystems() {
 
     // Note that although a number being passed as a parameter into a function may look like a numeral, it may be a string value 
     // instead, and may need to be converted to an integer. 
+
+    fetch('data.json')
+        .then(function(res){
+            return res.json()
+            //console.log('data: ', res.json())
+        })
+        .then (function(data){
+            console.log('data: ', data)
+            const myArr = data.systems.filter((item) => {
+                return item.parentID === parentId
+            })
+            console.log('myArr: ', myArr)
+            populateDD(myArr, domElement)
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+
+
 }
 
-function populateDD() {
+function populateDD(arr, domElement) {
     // This function receives the array and DOM element from the "getSystems()" function (above). This function should fill the 
     // appropriate DOM element with options from which the user can select. However, since that DOM element has an "onChange" event, this
     // function first needs to add an option that says "Select an Item". Then use a looping mechanism (week one) to create the rest of  
     // the select element's options using the sysName and sysID from the passed in array (see week five).
+
+    for (i = 0; i < arr.length; i++) {
+        // created items in the select list
+        let myOption = new Option(arr[i].sysName, arr[i].sysID)
+        domElement.options[i] = myOption
+    }
+
 }
 
-document.querySelector('#system').addEventListener(
+//document.querySelector('#system').addEventListener(
     // This eventListener responds to a change to the "system" select element by passing the selected value from the "system" element 
     // to the "getSystems()" function along with the "subSystem" DOM element so that the second drop-down list is populated with the 
     // appropriate sub-systems from the data.json file.
-)
+//)
--- week2/assignments/warehouse/warehouse.js
+++ week2/assignments/warehouse/warehouse.js
@@ -23,17 +23,67 @@ const parts = [
 ]
 
 // list of each part number and qty for check-off in the "detailsList" element
+let details = ''
+parts.forEach(item => details += `<input type="checkbox">&nbsp; ${item.qty} (${item.partNbr}) - ${item.partDescr}<br/>`)
+document.querySelector("#detailsList").innerHTML = details
 
 // if parts requiring special handling exist (in aisle B3), list of items needing 
 // special packaging in the "specialPackaging" element, else remove element
+const specialPackaging = parts.filter((item) => {
+    return item.aisle ==='B3'
+})
+if(specialPackaging.length > 0) {
+    let special = 'Special Packaging required<br />'
+    var specialmap = specialPackaging.map((item) => {
+        special += `Item ${item.partNbr} / Qty: ${item.qty}<br/>`
+    })
+    document.querySelector("#specialPackaging").innerHTML = special
+} else {
+    document.querySelector("#specialPackaging").remove()
+}
 
 // if hazardous parts exist (in aisle J4), let employee know in the "hazardousMaterials"
 // element and remind them to get gloves, else remove element
+const hazardousMaterials = parts.filter((item) => {
+    return item.aisle ==='J4'
+})
+if(hazardousMaterials.length > 0) {
+    let special = 'Gloves required<br />'
+    var specialmap = hazardousMaterials.map((item) => {
+        special += `Item ${item.partNbr} / Qty: ${item.qty}<br/>`
+    })
+    document.querySelector("#hazardousMaterials").innerHTML = special
+} else {
+    document.querySelector("#hazardousMatierals").remove()
+}
 
 // if all items in the order are small parts (aisle H1), then let employee know that they should take 
 // a basket and go dirctly to aisle H1
+const smallItemsOnly = parts.every((item) => {
+    return item.aisle ==='H1'
+})
+if(smallItemsOnly){
+    document.querySelector("#smallItemsOnly").innerHTML = "Small items go to H1. Make sure to take basket with you"
+    }
+else {
+    document.querySelector("#smallItemsOnly").remove()
+}
+
 
 // if there are large items (anthing in aisles S, T, or U), then let the employee know in the "forkiftNeeded"
 // element that they will need to reserve a forklift, else remove the element
+const forkliftNeeded = parts.some((item) => {
+    return (item.aisle === 'S' || item.aisle === 'T' || item.aisle === 'U')
+})
+    if(forkliftNeeded){
+        document.querySelector("#forkliftNeeded").innerHTML = "Please reserve a forklift."
+    }
+    else {
+        document.querySelector("#forkliftNeeded").remove()
+    }
 
 // sum up the total number of parts and append that number to the text already in "totalItems" element
+const totalItems = parts.reduce((total, item) => {
+    return item.qty + total
+}, 0)
+    document.querySelector("#totalItems").innerHTML = totalItems
